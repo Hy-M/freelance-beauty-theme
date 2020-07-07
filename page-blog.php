db9b18ee3f105183ddffd54a27c5be8271d5b173
Hy-M <humayraa.m@outlook.com>
2020-07-07 20:05:48 +0100

    add pageblog with pagination

---
+++ page-blog.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+		<section id="banner" class="banner--xs">
+			<h1 class="banner--heading banner--heading-emphasis">
+				Welcome to my blog
+			</h1>
+			<p class="banner--tagline">
+				Beauty and business related content
+			</p>
+		</section>
+		<main class="main--bloglist">
+			<section class="section posts">
+                <!-- <h2 class="h2">HY digital blog</h2> -->
+				<section class="section cards">
+				<?php 
+					$current_page = get_query_var('paged') ? get_query_var('paged'):1;
+                    $args = array(
+						'post_type' => 'post',
+						'posts_per_page' => 6,
+						'paged' => $current_page
+                    );
+
+					$blogposts = new WP_Query($args);
+                    
+                    while($blogposts->have_posts()) {
+                        $blogposts->the_post();
+
+                ?>
+					<div class="cards--card">
+						<div class="cards--card-image">
+							<a href="<?php the_permalink();?>">
+								<img src="<?php echo get_the_post_thumbnail_url(get_the_ID());?>" alt="" />
+							</a>
+						</div>
+						<div class="cards--card-description">
+							<a href="<?php the_permalink();?>">
+								<h4 class="h4">
+									<?php the_title();?>
+								</h4>
+							</a>
+							<p class="p--subtext">By <?php the_author();?> on <?php the_time('F j, Y'); ?> in <a href="#"><?php echo get_the_category_list(', ');?></a></p>
+							<p class="p">
+                                <?php echo wp_trim_words(get_the_excerpt(), 15); ?>
+							</p>
+							<a class="btn--secondary cards--card-btn" href="<?php the_permalink(); ?>">Read more</a>
+						</div>
+                    </div>
+                    
+					<?php } wp_reset_query();?>
+				</section>
+			</section>
+			<section class="section--pagination">
+					<?php 
+						echo paginate_links( 
+							array(
+								"current" => $current_page,
+								"total" => $blogposts->max_num_pages
+							) 
+						);
+					?>
+			</section>
+		</main>
+<?php get_footer(); ?>
