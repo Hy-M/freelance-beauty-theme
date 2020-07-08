8655f66bc499bf0338c3d5c181a69aba787d254b
Hy-M <humayraa.m@outlook.com>
2020-07-08 14:57:16 +0100

    style comment FORM

--- single.php
+++ single.php
@@ -12,33 +12,34 @@
 		</p>
 	</section>
 	<main>
-    
 		<section class="section post">
 			<p class="p post--text">
 				<?php the_content();?>
 			</p>
 		</section>
-		<section class="section section--comments">
-			<h4 class="h4 comments--heading">Comments</h4>
+		<section class="section--comments">
+            <section class="comments--form">
             <?php 
                 $fields = array(
-                    'author' => '<input placeholder="Your name" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author']) . '" size="30"' . $aria_req . '/></p>',
-                    'email' => '<input placeholder="Your email address" id="email" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email']) . '" size="30"' . $aria_req . '/></p>',
+                    'author' => '<input class="comments--form-input" placeholder="Your name" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author']) . '" size="30"' . $aria_req . '/></p>',
+                    'email' => '<input class="comments--form-input" placeholder="Your email address" id="email" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email']) . '" size="30"' . $aria_req . '/></p>',
                 );
 
                 $args = array(
                     'title_reply' => 'Share your thoughts',
                     'fields' => $fields,
-                    'comment_field' => '<textarea placeholder="Your comments" id="comment" name="comment" cols="45" rows="8" aria-required="true">' . '</textarea>',
-                    'comment_notes_before' => '<p class="comment-notes">Your email address will not be displayed. All fields are required.</p>'
+                    'comment_field' => '<textarea class="comments--form-textarea" placeholder="Your comments" id="comment" name="comment" cols="45" rows="8" aria-required="true">' . '</textarea>',
+                    'comment_notes_before' => '<p class="p--subtext comments--form-text">Your email address will not be displayed. All fields are required.</p>'
                 );
             
                 comment_form($args);
                 $comments_number = get_comments_number();
-
+                ?>
+                </section>
+                <?php
                 if($comments_number != 0) { ?>
                     <section class="comments">
-                        <h3 class="h3">What others are saying</h3>
+                        <h3 class="h3 comments--heading">What others are saying</h3>
                         <ol class="comments--list">
                             <?php 
                                 $comments = get_comments(array(
@@ -58,18 +59,3 @@
 	</main>
 
 <?php get_footer(); ?>
-
-<!-- 
-<?php 
-    while(have_posts()) {
-        the_post();
-
-        ?>
-        <h2><?php the_title();?></h2>
-        posted by:  <?php the_author();?>
-        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>"
-        <?php the_content();
-        comment_form();
-    }
-
-?> -->
