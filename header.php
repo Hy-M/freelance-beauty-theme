d84ed65724e316d2b564daedb5db0e25369a3030
Hy-M <humayraa.m@outlook.com>
2020-07-09 16:47:03 +0100

    adding logo and styling

--- header.php
+++ header.php
@@ -20,8 +20,10 @@
 			<nav>
 				<section id="logo">
 					<a href="<?php echo site_url(''); ?>">
-						<p>HY digital</p>
+						<!-- <p>HY digital</p> -->
+						<img class="logo--img" src="<?php echo get_template_directory_uri(); ?>/img/hydev.png" alt="">
 					</a>
+					
 				</section>
 				<div id="menu--icon">
 					<i class="fas fa-bars"></i>
